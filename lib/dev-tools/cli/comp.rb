f2b59634ab857896b0a0429d3864884b3075eb63
Metallion <akieckens@axsh.net>
2013-06-03 18:38:16 +0900

    Applied the enable components syntax sugar to the component cli

--- lib/dev-tools/cli/comp.rb
+++ lib/dev-tools/cli/comp.rb
@@ -6,20 +6,12 @@ module DevTools::Cli
 
     desc "start #{DevTools::Constants::Config::PROJECTS.join("|")} [options]", "Starts components on nodes."
     def start(project)
-      DevTools::Node.enabled.each {|node|
-        node.comps[project].each { |comp|
-          comp.start
-        }
-      }
+      DevTools::Component.enabled(project).each {|comp| comp.start}
     end
 
     desc "stop #{DevTools::Constants::Config::PROJECTS.join("|")}", "Stops components on nodes."
     def stop(project)
-      DevTools::Node.enabled.each { |node|
-        node.comps[project].each { |comp|
-          comp.stop
-        }
-      }
+      DevTools::Component.enabled(project).each {|comp| comp.stop}
     end
   end
 end
