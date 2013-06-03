ded734ec5eb2e30daeb3f29a08624eb05baa24c2
Metallion <akieckens@axsh.net>
2013-06-03 13:05:06 +0900

    Added the first commit with some general directory structure and Gemfile

---
+++ lib/dev-tools.rb
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+module DevTools
+  module Config
+    autoload :Node, 'dev-tools/config/node'
+    autoload :Comp, 'dev-tools/config/comp'
+  end
+  module Cli
+    autoload :Root, 'dev-tools/cli/root'
+  end
+end
