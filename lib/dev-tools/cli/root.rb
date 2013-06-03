c3ce8dcd0eaa7f8e5f75fab327879d935a7a2392
Metallion <akieckens@axsh.net>
2013-06-03 15:43:26 +0900

    Added option for starting without the bridge or without starting the nodes

--- lib/dev-tools/cli/root.rb
+++ lib/dev-tools/cli/root.rb
@@ -2,14 +2,18 @@
 
 module DevTools::Cli
   class Root < Thor
-    desc "start", "Starts all nodes"
+    desc "start [options]", "Starts all nodes"
+    method_option :no_bridge, :type => :boolean, :default => false, :desc => "Don't create the bridge."
+    method_option :no_nodes, :type => :boolean, :default => false, :desc => "Don't start the nodes."
     def start
-      c = DevTools.conf
-      bridge = DevTools::Bridge.new(c.bridge_ip,c.bridge_ip_prefix,c.bridge_devname)
-      bridge.create
-      bridge.share_internet(c.internet_nic)
+      unless options[:no_bridge]
+        c = DevTools.conf
+        bridge = DevTools::Bridge.new(c.bridge_ip,c.bridge_ip_prefix,c.bridge_devname)
+        bridge.create
+        bridge.share_internet(c.internet_nic)
+      end
 
-      DevTools::Node.enabled.each {|node| node.start }
+      DevTools::Node.enabled.each {|node| node.start } unless options[:no_nodes]
     end
 
     desc "stop", "Stops all nodes"
