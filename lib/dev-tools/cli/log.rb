661accaf34fdce495cbd951d7dff2805ef0fbf29
Metallion <akieckens@axsh.net>
2013-06-05 16:54:37 +0900

    Added feature to show log by node

--- lib/dev-tools/cli/log.rb
+++ lib/dev-tools/cli/log.rb
@@ -4,12 +4,19 @@ module DevTools::Cli
   class Log < Thor
     namespace 'log'
 
-    desc "show #{DevTools::Constants::Config::PROJECTS.join("|")}", "Show the "
+    desc "show #{DevTools::Constants::Config::PROJECTS.join("|")} [options]", "Show the "
+    method_option :node, :type => :string, :desc => "The name of a node whose log to show.", :aliases => :n
     def show(project)
       screen = DevTools::Screen.new(project)
       screen.init
 
-      DevTools::Component.enabled(project).each {|comp|
+      comps = if options[:node]
+        DevTools::Node.new(options[:node]).comps[project]
+      else
+        DevTools::Component.enabled(project)
+      end
+      comps.each{ |comp|
+        next unless comp.show_log?
         screen.open("#{comp.node.name}-#{comp.name}","tail -f #{comp.log_path}")
       }
 
