7877a08604d5e9c34e7d428324eda216e87c6a27
Metallion <akieckens@axsh.net>
2013-06-05 16:54:04 +0900

    Added feature to start components by node

--- lib/dev-tools/cli/comp.rb
+++ lib/dev-tools/cli/comp.rb
@@ -5,13 +5,27 @@ module DevTools::Cli
     namespace :comp
 
     desc "start #{DevTools::Constants::Config::PROJECTS.join("|")} [options]", "Starts components on nodes."
+    method_option :node, :type => :string, :desc => "The name of a node whose components we want to start.", :aliases => :n
     def start(project)
-      DevTools::Component.enabled(project).each {|comp| comp.start}
+      if options[:node]
+        DevTools::Node.new(options[:node]).comps[project].each { |comp|
+          comp.start
+        }
+      else
+        DevTools::Component.enabled(project).each {|comp| comp.start}
+      end
     end
 
-    desc "stop #{DevTools::Constants::Config::PROJECTS.join("|")}", "Stops components on nodes."
+    desc "stop #{DevTools::Constants::Config::PROJECTS.join("|")} [options]", "Stops components on nodes."
+    method_option :node, :type => :string, :desc => "The name of a node whose components we want to start.", :aliases => :n
     def stop(project)
-      DevTools::Component.enabled(project).each {|comp| comp.stop}
+      if options[:node]
+        DevTools::Node.new(options[:node]).comps[project].each { |comp|
+          comp.start
+        }
+      else
+        DevTools::Component.enabled(project).each {|comp| comp.stop}
+      end
     end
   end
 end
